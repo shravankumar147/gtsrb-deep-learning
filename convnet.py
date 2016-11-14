12ebf4d81b44ea768ca369b1cf5434c7cbfeff06
tallosan <andrew.tallos@mail.utoronto.ca>
2016-11-14 12:47:31 -0500

    Update: Can now load save/load existing models. Currently this runs with 6% validation error.

--- convnet.py
+++ convnet.py
@@ -10,6 +10,8 @@ import theano.tensor as T
 
 import lasagne as nnet
 
+import cPickle as pickle
+
 from data import *
 
 
@@ -18,63 +20,42 @@ class ConvNNet():
 	'''	Initialize a convolutional neural network.
 		We'll use the Glorot initialization for our weights.
 		ReLu for non-linearity, and a 43-way Softmax for our classifier.
-		N.B. -- Each layer will be have a suffix from a-z, with an
-			abbreviation as the suffix.
-		
-			E.g. self.b_cnv == Second(b) layer is Convolutional.
 		Args:
 			inpt_shape: The dimensions of the input tensor.
 				    (__, channels, width, height)
 	'''
-	def __init__(self, inpt_shape):
-
+	def __init__(self, inpt_shape, use_existing=False):
 		
 		self.inpt	= T.tensor4('inputs')
 		self.y		= T.ivector('y')
-		
-		self.W		= nnet.init.GlorotUniform()
-		
-		self.nonlin 	= nnet.nonlinearities.rectify
-		self.classifier = nnet.nonlinearities.softmax
-		
-		# Input layer.
-		self.a_in  = nnet.layers.InputLayer(shape=inpt_shape,
-						    inpt_var=self.inpt
-		)
-		
-		# Convolutional Layers.
-		self.b_cnv = nnet.layers.Conv2DLayer(self.a_in,
-						     num_filters=32,
-						     filter_size=(5,5),
-						     nonlinearity=self.nonlin,
-						     W=self.W
-		)
-		self.c_cnv = nnet.layers.Conv2DLayer(self.b_cnv,
-						     num_filters=32,
-						     filter_size=(5,5),
-						     nonlinearity=self.nonlin
-		)
-		self.d_cnv = nnet.layers.Conv2DLayer(self.c_cnv,
-						     num_filters=32,
-						     filter_size=(5,5),
-						     nonlinearity=self.nonlin
-		)
+			
+		l0 = nnet.layers.InputLayer(shape=inpt_shape,
+						 input_var=self.inpt)
+		l1 = nnet.layers.Conv2DLayer(
+				l0, num_filters=32, filter_size=(5,5),
+				nonlinearity=nnet.nonlinearities.rectify,
+				W=nnet.init.GlorotUniform())
+		l3 = nnet.layers.Conv2DLayer(
+				l1, num_filters=32, filter_size=(5,5),
+				nonlinearity=nnet.nonlinearities.rectify)
+		l3a = nnet.layers.DenseLayer(
+				nnet.layers.dropout(l3, p=0.5),
+				num_units=50,
+				nonlinearity=nnet.nonlinearities.rectify)
+		self.cnet = nnet.layers.DenseLayer(
+				nnet.layers.dropout(l3a, p=0.5),
+				num_units=43, 
+				nonlinearity=nnet.nonlinearities.softmax)
 
-		# Fully Connected / Dense layers.
-		self.e_fc  = nnet.layers.DenseLayer(self.d_cnv,
-						    num_units=30,
-						    nonlinearity=self.nonlin
-		)
-	
-		# Classifier.
-		self.cnet = nnet.layers.DenseLayer(self.e_fc,
-						    num_units=43,
-						    nonlinearity=self.classifier
-		)
+		# Load in parameters from an existing model.
+		if use_existing:
+			print '\tloading existing model.'
+			with np.load('gtsrb-model.npz') as fp:
+				params = [fp['arr_%d' % i]
+						for i in range(len(fp.files))]
+				nnet.layers.set_all_param_values(self.cnet, params)
 
 		self.params = nnet.layers.get_all_params(self.cnet, trainable=True)
-		print self.params
-
 
 	'''	Train the model with the given parameters.
 		Args:
@@ -91,44 +72,68 @@ class ConvNNet():
 		
 		n_train_batches  = x_train.shape.eval()[0] / batch_size
 		n_val_batches	 = x_val.shape.eval()[0] / batch_size
-		x_test_batches	 = x_test.shape.eval()[0] / batch_size
-
+		n_test_batches	 = x_test.shape.eval()[0] / batch_size
+			
 		# Create a cost expression.
-		pred 	  = nnet.layers.get_output(self.cnet)
-		cost	  = (nnet.objectives.categorical_crossentropy(
-				pred, self.y)).mean()
-		updates	  = nnet.updates.nesterov_momentum(
-					cost, self.params,
-					learning_rate=eta, momentum=rho)
+		prediction = nnet.layers.get_output(self.cnet)
+		loss = nnet.objectives.categorical_crossentropy(
+				prediction, self.y)
+		loss = loss.mean()	
 		
+		# Update rule. We'll use Nesterov Momentum to speed up convergence.
+		updates = nnet.updates.nesterov_momentum(loss, self.params,
+				learning_rate=eta, momentum=rho)
+	
 		test_pred = nnet.layers.get_output(self.cnet, determinstic=True)
-		test_loss = nnet.objectives.categorical_crossentropy(pred, self.y)
-		test_acc  = T.mean(T.eq(T.argmax(test_pred, axis=1), self.y),
+		test_loss = nnet.objectives.categorical_crossentropy(
+				test_pred, self.y)
+		test_loss = test_loss.mean()
+		test_acc = T.mean(T.eq(T.argmax(test_pred, axis=1), self.y),
 				dtype=theano.config.floatX)
-
-		train_model    = theano.function(inputs=[self.inpt, self.y],
-						 outputs=cost,
-						 updates=updates
+		
+		# Definitions for training and validating our model.
+		train_model = theano.function(
+				[self.inpt, self.y], loss, updates=updates
 		)
-		validate_model = theano.function(inputs=[self.inpt, self.y],
-						 outputs=[test_loss, test_acc]
+		val_model = theano.function(
+				[self.inpt, self.y], [test_loss, test_acc]
 		)
 
 		print '\n\tbeginning training ...'
 		for epoch in xrange(epochs):
-			for batch_index in xrange(n_train_batches):
-				x_batch = x_train[ i * batch_size: 
-						  (i + 1) * batch_size ]
-				y_batch = y_train[ i * batch_size:
-						  (i + 1) * batch_size ]
-				x = train_model(x_batch, y_batch)
 			
-			val_xb = x_val[:20]
-			val_yb = y_val[:20]
-			err, acc = validate_model(val_xb, val_yb)
+			train_err, train_batches = 0, 0
+			for batch_index in xrange(n_train_batches):
+				x_batch = x_train[batch_index * batch_size: 
+						  (batch_index + 1) * batch_size]
+				y_batch = y_train[batch_index * batch_size:
+						  (batch_index + 1) * batch_size]
+				train_err += train_model(
+						x_batch.eval(), y_batch.eval())
+				train_batches += 1
 
+			val_err, val_acc, val_batches = 0, 0, 0
+			for batch_index in xrange(n_val_batches):
+				x_batch = x_val[batch_index * batch_size:
+						(batch_index + 1) * batch_size]
+				y_batch = y_val[batch_index * batch_size:
+						(batch_index + 1) * batch_size]
+				err, acc = val_model(
+						x_batch.eval(), y_batch.eval())
+				val_err += err
+				val_acc += acc
+				val_batches += 1
+			
+			print 'Epoch ', epoch, 'of ', epochs, \
+				'\n\ttraining error: ',train_err/train_batches,\
+			       '\n\tvalidation error: ', val_err/val_batches,\
+			       '\n\tvalidation_accuracy: ', val_acc/val_batches * 100
+			
+			np.savez('gtsrb-model.npz',
+					*nnet.layers.get_all_param_values(self.cnet))
+
+		
 # Load in the data.
-print '\n\tloading data ...'
 data = load_dataset()
 network_in = {
 		'training': data[0],
@@ -136,20 +141,14 @@ network_in = {
 		'test': data[2]
 	     }
 
-print 'Training: ', network_in['training'][0].shape.eval()[0], \
-		    network_in['training'][1].shape.eval()
-print 'Validation: ', network_in['validation'][0].shape.eval()
-print 'Test: ', network_in['test'][0].shape.eval()
-
-print '\n\tcreating convolutional neural network.'
-gtsrb_convnet = ConvNNet(inpt_shape=(None, 3, 15,15))
-print '\n\tCNN initialized. beginning training ...'
-BATCH_SIZE = 120
+# Create the Convn Net.
+gtsrb_convnet = ConvNNet(inpt_shape=(None, 3, 15,15), use_existing=True)
+BATCH_SIZE = 1120
 gtsrb_convnet.train(
 				dataset=network_in,
 				batch_size=BATCH_SIZE,
-				epochs=500,
-				eta=0.01,
+				epochs=300,
+				eta=0.0001,
 				rho=0.9
 			)
 
